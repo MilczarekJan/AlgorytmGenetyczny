82514c5dfa3c48c2f8a00f54e1aed3234a024cbd
Jan <01169598@pw.edu.pl>
2024-10-16 15:21:48 +0100

    Pierwsze UI

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ from tkinter import ttk
 root = Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+ttk.Label(frm, text="Mutation chance:", padding=10).grid(column=0, row=0)
+ttk.Label(frm, text="Crossover chance:", padding=10).grid(column=1, row=0)
+ttk.Label(frm, text="Max number of generations:", padding=10).grid(column=2, row=0)
+
+Mutation = ttk.Entry(frm).grid(column=0, row=1, padx=15)
+Crossover = ttk.Entry(frm).grid(column=1, row=1, padx=15)
+Generations = ttk.Entry(frm).grid(column=2, row=1, padx=15)
+#ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
 root.mainloop()
