6c22844ff5cff84e8f1b7498e422d3824bfe5ed2
Jan <01169598@pw.edu.pl>
2024-11-11 19:42:36 +0100

    11.11.2024

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -1,3 +1,4 @@
+import array
 import matplotlib.pyplot as plt
 import numpy as np
 import random
@@ -25,104 +26,94 @@ class GeneticAlgorithm:
             fitness_function = -0.2 * x**2 + 6*x + 7
             return fitness_function
 
-    def reproduction(self, population):
-        # Obliczanie przystosowania ka¿dego osobnika w populacji
-        fitness_values = np.array([self.fitness(ind) for ind in population])
-    
-        # Obliczanie ca³kowitego przystosowania
+    def reproduction(self, population, fitness_values):
         total_fitness = np.sum(fitness_values)
-    
-        # Obliczanie prawdopodobieñstw selekcji dla ka¿dego osobnika
         selection_probabilities = fitness_values / total_fitness
-    
-        # Skumulowane prawdopodobieñstwa do selekcji
         cumulative_probabilities = np.cumsum(selection_probabilities)
-    
-        # Nowa populacja na podstawie selekcji ruletki
         new_population = []
         for _ in range(len(population)):
-            # Wybór osobnika na podstawie skumulowanych prawdopodobieñstw
             r = np.random.random()
             selected_individual_index = np.searchsorted(cumulative_probabilities, r)
             new_population.append(population[selected_individual_index])
     
         return np.array(new_population)
 
-    def mutate(self, individual):
+    def mutation(self, population):
 
-        if individual < self.FunctionDomain[0] or individual > self.FunctionDomain[1]:
-            raise ValueError("Osobnik jest poza dozwolonym zakresem.")
+        returned_population = []
 
-        # Konwersja liczby na 6-bitow¹ reprezentacjê binarn¹
-        if individual < 0:
-            bin_repr = format(32 + individual, '06b')  # Kodowanie ujemnych liczb na 6 bitach
-        else:
-            bin_repr = format(individual, '06b')
-
-        # Mutacja bitów
-        mutated_bits = []
-        for bit in bin_repr:
-            if random.random() < self.MutationChance:
-                mutated_bits.append('0' if bit == '1' else '1')
+        for individual in population:
+            if individual < 0:
+                bin_repr = format(32 + individual, '06b')
             else:
-                mutated_bits.append(bit)
+                bin_repr = format(individual, '06b')
 
-        # Sk³adanie zmienionych bitów
-        mutated_bin_repr = ''.join(mutated_bits)
-        mutated_integer_part = int(mutated_bin_repr, 2)
-        if mutated_bin_repr[0] == '1':  # Sprawdzenie bitu znaku
-            mutated_integer_part -= 32
+            mutated_bits = []
+            for bit in bin_repr:
+                if random.random() < self.MutationChance:
+                    mutated_bits.append('0' if bit == '1' else '1')
+                else:
+                    mutated_bits.append(bit)
 
-        # Zwrócenie osobnika po mutacji, ograniczone do przedzia³u
-        if mutated_integer_part > self.FunctionDomain[1]:
-            return self.FunctionDomain[1]
-        elif mutated_integer_part < self.FunctionDomain[0]:
-            return self.FunctionDomain[0]
-        else:
-            return mutated_integer_part
+            mutated_bin_repr = ''.join(mutated_bits)
+            mutated_integer_part = int(mutated_bin_repr, 2)
+            if mutated_bin_repr[0] == '1':
+                mutated_integer_part -= 32
 
-    def crossover(self, parent1, parent2):
-        if random.random() < self.CrossoverChance:
-            # Zamiana liczby na reprezentacjê binarn¹ (6-bitow¹ dla przedzia³u od -1 do 31)
-            bin_repr1 = format(int(parent1) if parent1 >= 0 else 32 + int(parent1), '06b')
-            bin_repr2 = format(int(parent2) if parent2 >= 0 else 32 + int(parent2), '06b')
+            if mutated_integer_part > self.FunctionDomain[1]:
+                returned_population.append(self.FunctionDomain[1])
+            elif mutated_integer_part < self.FunctionDomain[0]:
+                returned_population.append(self.FunctionDomain[0])
+            else:
+                returned_population.append(mutated_integer_part)
+        return returned_population
 
-            # Wybór losowego punktu krzy¿owania od 1 do 5 (zawsze 6 bitów, wiêc pe³ny zakres to 1-5)
-            crossover_point = random.randint(1, 5)
+    def crossover(self, population):
+        mating_population = []
+        stagnant_population = []
+        mating_population_to_return = []
+        mating_size = int(self.CrossoverChance*len(population))
 
-            # Tworzenie nowych osobników przez zamianê bitów
-            new_bin_repr1 = bin_repr1[:crossover_point] + bin_repr2[crossover_point:]
-            new_bin_repr2 = bin_repr2[:crossover_point] + bin_repr1[crossover_point:]
+        mating_population = list(population[:mating_size])
+        stagnant_population = list(population[mating_size:])
 
-            # Konwersja binarnej reprezentacji na liczby ca³kowite
-            new_parent1 = int(new_bin_repr1, 2) - (32 if new_bin_repr1[0] == '1' else 0)
-            new_parent2 = int(new_bin_repr2, 2) - (32 if new_bin_repr2[0] == '1' else 0)
+        for parent1 in mating_population:
+            
+            mating_population.remove(parent1)
+            if mating_population:
+                parent2 = random.choice(mating_population)
+                mating_population.remove(parent2)
 
-            return new_parent1, new_parent2
-        else:
-            # Jeœli krzy¿owanie nie zachodzi, zwracamy rodziców bez zmian
-            return parent1, parent2
+                bin_repr1 = format(int(parent1) if parent1 >= 0 else 32 + int(parent1), '06b')
+                bin_repr2 = format(int(parent2) if parent2 >= 0 else 32 + int(parent2), '06b')
+
+                crossover_point = random.randint(1, 5)
+
+                new_bin_repr1 = bin_repr1[:crossover_point] + bin_repr2[crossover_point:]
+                new_bin_repr2 = bin_repr2[:crossover_point] + bin_repr1[crossover_point:]
+
+                new_parent1 = int(new_bin_repr1, 2) - (32 if new_bin_repr1[0] == '1' else 0)
+                new_parent2 = int(new_bin_repr2, 2) - (32 if new_bin_repr2[0] == '1' else 0)
+
+                mating_population_to_return.append(new_parent1)
+                mating_population_to_return.append(new_parent2)
+
+        mating_population_to_return.extend(stagnant_population)
+        return mating_population_to_return
 
     def startgenetic(self):
-        # Generacja losowej pocz¹tkowej populacji
         population = np.random.randint(self.FunctionDomain[0], self.FunctionDomain[1] + 1, self.StartingPopulation)
 
-        # Statystyki do zbierania wyników
         min_fitness_per_gen = []
         avg_fitness_per_gen = []
         max_fitness_per_gen = []
 
         for generation in range(self.NumberOfGenerations):
-            population = self.reproduction(population)
-            for i in range(0, len(population), 2):
-                if i + 1 < len(population):
-                    parent1, parent2 = population[i], population[i + 1]
-                    offspring1, offspring2 = self.crossover(parent1, parent2)
-                    offspring1 = self.mutate(offspring1)
-                    offspring2 = self.mutate(offspring2)
-                    population[i], population[i + 1] = offspring1, offspring2
-            # Ocena przystosowania ka¿dego osobnika
-            fitness_values = np.array([self.fitness(ind) for ind in population])
+            np.random.shuffle(population) #zachowanie losowoœci
+            fitness_values = np.array([self.fitness(ind) for ind in population]) # Ocena przystosowania ka¿dego osobnika
+            population = self.reproduction(population, fitness_values) #reprodukcja
+            population = self.crossover(population) #krzy¿owanie
+            population = self.mutation(population) #mutacja
             # Zbieranie statystyk przystosowania
             min_fitness_per_gen.append(np.min(fitness_values))
             avg_fitness_per_gen.append(np.mean(fitness_values))
@@ -132,32 +123,26 @@ class GeneticAlgorithm:
 
     def plot_fitness(self, min_fitness, avg_fitness, max_fitness):
         generations = range(self.NumberOfGenerations)
-
-        # Tworzenie uk³adu podzielonych osi (4 wiersze na wykresy)
         fig, axs = plt.subplots(4, 1, figsize=(8, 12))
 
-        # Tworzenie wykresu minimalnego przystosowania
         axs[0].plot(generations, min_fitness, label="Min Fitness", color='blue')
         axs[0].set_xlabel("Generation")
         axs[0].set_ylabel("Min Fitness")
         axs[0].legend()
         axs[0].set_title("Min Fitness over Generations")
 
-        # Tworzenie wykresu œredniego przystosowania
         axs[1].plot(generations, avg_fitness, label="Avg Fitness", color='green')
         axs[1].set_xlabel("Generation")
         axs[1].set_ylabel("Avg Fitness")
         axs[1].legend()
         axs[1].set_title("Avg Fitness over Generations")
 
-        # Tworzenie wykresu maksymalnego przystosowania
         axs[2].plot(generations, max_fitness, label="Max Fitness", color='red')
         axs[2].set_xlabel("Generation")
         axs[2].set_ylabel("Max Fitness")
         axs[2].legend()
         axs[2].set_title("Max Fitness over Generations")
 
-        # Rysowanie funkcji badanej na czwartej osi
         x = np.linspace(self.FunctionDomain[0], self.FunctionDomain[1], 100)
         y = -0.2 * x**2 + 6*x + 7
         axs[3].plot(x, y, label="Examined Function", color='purple')
@@ -166,7 +151,6 @@ class GeneticAlgorithm:
         axs[3].legend()
         axs[3].set_title("Examined Function")
 
-        # Dopasowanie wykresów dla lepszej estetyki
         plt.tight_layout()
         plt.show()
 
